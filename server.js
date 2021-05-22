6393c409b145fe0cc4f7757e83664df8b93fb17d
Nirbhay1910 <nirbhay1910@gmail.com>
2021-05-22 15:21:45 +0530

    Added sockets for video call

--- server.js
+++ server.js
@@ -16,4 +16,14 @@ const peerServer = ExpressPeerServer(server, {
 });
 app.use('/peerjs', peerServer);
 
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit('user-connected', userId);
+    socket.on('disconnect', () => {
+      socket.to(roomId).emit('user-disconnected', userId);
+    });
+  });
+});
+
 server.listen(5000);
